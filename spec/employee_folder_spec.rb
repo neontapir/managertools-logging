51bd27a61f134cad4d641c9524be93431b12ab70
Chuck Durfee <charles.durfee@sap.com>
2016-12-21 08:30:11 -0700

    enhance test case with more Unicode downcase conversions

--- spec/employee_folder_spec.rb
+++ spec/employee_folder_spec.rb
@@ -5,18 +5,18 @@ describe EmployeeFolder do
   context "in accented characters context" do
     before(:all) do
       Dir.mkdir("data") unless Dir.exist? "data"
-      Dir.mkdir("data/āccented")
+      Dir.mkdir("data/āccéñťèð")
     end
 
     after(:all) do
-      Dir.rmdir("data/āccented/ezel-çeçek")
-      Dir.rmdir("data/āccented")
+      Dir.rmdir("data/āccéñťèð/ezel-çeçek")
+      Dir.rmdir("data/āccéñťèð")
     end
 
     it "should create folder with accented characters" do
-      employee = Employee.new({team: 'Āccented', first: 'Ezel', last: 'Çeçek'})
+      employee = Employee.new({team: 'ĀccÉñŤÈÐ', first: 'Ezel', last: 'Çeçek'})
       folder = EmployeeFolder.new employee
-      expected_path = "data/āccented/ezel-çeçek"
+      expected_path = "data/āccéñťèð/ezel-çeçek"
       expect(folder.path).to eq(expected_path)
 
       folder.ensure_exists
