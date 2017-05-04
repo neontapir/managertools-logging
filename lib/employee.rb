4a392aabac7933915da0ad3d54e8e0c30cb9106c
Chuck Durfee <charles.durfee@sap.com>
2017-05-04 16:51:18 -0600

    extract EmployeeFinder from Employee

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,17 +1,16 @@
-require 'pathname'
 require 'facets/string/titlecase'
 
+require_relative 'employee_finder'
 require_relative 'employee_folder'
-require_relative 'log_file'
 require_relative 'mt_data_formatter'
 require_relative 'path_splitter'
-require_relative 'settings'
 
 # Represents a team member, a person assigned to a team
 # @attr_reader [String] team the name of the team the person belongs to
 # @attr_reader [String] first the first name of the team member
 # @attr_reader [String] last the last name of the team member
 class Employee
+  extend EmployeeFinder
   extend MtDataFormatter
   extend PathSplitter
 
@@ -24,75 +23,6 @@ class Employee
     @team = params.fetch(:team)
     @first = params.fetch(:first)
     @last = params.fetch(:last)
-  end
-
-  # Parse the path as though it is an employee spec and return the result
-  #
-  # @param [String] dir the location of a person
-  # @return [Hash] the employee data represented by the location
-  def self.parse_dir(dir)
-    paths = split_path dir
-    _root, team, name = paths
-    { team: team }.merge(parse_name(name))
-  end
-
-  # Parse a string as though it is part of an employee spec and return the result
-  def self.parse_name(name)
-    first, last = name.tr('-', ' ').titlecase.strip.split(/\s+/)
-    { first: first, last: last }
-  end
-
-  # Given a part of employee data, find the first matching employee
-  #
-  # @param [String] key the lookup data needed to find the employee
-  # @return [Employee] an Employee object
-  def self.find(key)
-    root = EmployeeFolder.root
-    Dir.glob("#{root}/*/*") do |d|
-      next unless Dir.exist? d
-      if /#{key}/ =~ d.to_s
-        employee = parse_dir d
-        return Employee.new employee
-      end
-    end
-  end
-
-  # Description of method
-  #
-  # @param [Hash] person employee data
-  # @param [Symbol] type of employee, in practice generic or candidate
-  # @return [Hash] a specification of the employee
-  def self.get(person, type = :generic)
-    employee_spec = Employee.find person
-    employee = if employee_spec.nil?
-                 Employee.new(create_spec(type, parse_name(person)))
-               else
-                 employee_spec
-               end
-    employee
-  end
-
-  # Create a specification describing a person
-  #
-  # @param [String] type the type of entry
-  # @return [Hash] a specification of the employee
-  def self.create_spec(type, person)
-    result = {}
-    result[:team] = if type.to_sym.eql? :interview
-                      EmployeeFolder.candidates_root
-                    else
-                      obtain('Team', EmployeeFolder.candidates_root)
-                    end
-    result[:first] = obtain('First', person[:first] || 'Zaphod')
-    result[:last] = obtain('Last', person[:last] || 'Beeblebrox')
-    result
-  end
-
-  # Display the prompt, and get the element's value from the user
-  def self.obtain(prompt, default = '')
-    Settings.console.ask "#{prompt}: " do |answer|
-      answer.default = default
-    end
   end
 
   # Get the LogFile for the employee
---
+++ spec/employee_finder_spec.rb
@@ -0,0 +1,116 @@
+require 'ostruct'
+require 'stringio'
+require './lib/employee.rb'
+require './lib/settings.rb'
+require_relative 'captured_io'
+require_relative 'settings_helper'
+
+describe EmployeeFinder do
+  include CapturedIO
+  include SettingsHelper
+
+  subject { (Class.new { include EmployeeFinder }).new }
+
+  context 'with a non-existing employee (Red Panda)' do
+    it 'will prompt the user for a spec if the employee is not found' do
+      input = StringIO.new("Terrific Twosome of Toronto\n\Kit\nBaxter\n")
+      with_captured(input) do |_|
+        flying_squirrel = subject.get('Kit', :superhero)
+        expect(flying_squirrel.team).to eq('Terrific Twosome of Toronto')
+        expect(flying_squirrel.first).to eq('Kit')
+        expect(flying_squirrel.last).to eq('Baxter')        
+      end
+    end
+
+    it 'can create a spec with no input' do
+      defaults = { first: 'Zaphod', last: 'Beeblebrox', team: Settings.candidates_root }
+      input = StringIO.new("\n\n\n")
+      with_captured(input) do |_|
+        expect(subject.create_spec(:superhero, {})).to eq(defaults)
+      end
+    end
+
+    it 'can create a spec with give input' do
+      red_panda = {first: 'August', last: 'Fenwick', team: 'Terrific Twosome of Toronto'}
+      input = StringIO.new("Terrific Twosome of Toronto\nAugust\nFenwick\n")
+      with_captured(input) do |_|
+        expect(subject.create_spec(:superhero, {})).to eq(red_panda)
+      end
+    end
+
+    it 'will not prompt for an interview candidate' do
+      flying_squirrel = { first: 'Kit', last: 'Baxter', team: Settings.candidates_root }
+      input = StringIO.new("Kit\nBaxter\n")
+      with_captured(input) do |_|
+        expect(subject.create_spec(:interview, {})).to eq(flying_squirrel)
+      end
+    end
+  end
+
+  def is_correct?(employee, team, first, last)
+    expect(employee).not_to be_nil
+    employee_team = employee.instance_of?(Employee) ? employee.team : employee.fetch(:team)
+    expect(employee_team).to eq team
+    employee_first = employee.instance_of?(Employee) ? employee.first : employee.fetch(:first)
+    expect(employee_first).to eq first
+    employee_last = employee.instance_of?(Employee) ? employee.last : employee.fetch(:last)
+    expect(employee_last).to eq last
+  end
+
+  context 'when parsing an employee folder (Iron Man)' do
+    before(:all) do
+      FileUtils.mkdir_p('data/avengers/tony-stark')
+    end
+
+    after(:all) do
+      FileUtils.rm_r('data/avengers')
+    end
+
+    it 'extracts the data correctly' do
+      dir = Dir.new('data/avengers/tony-stark')
+      iron_man = subject.parse_dir(dir)
+      expect(is_correct? iron_man, 'avengers', 'Tony', 'Stark').to be_truthy
+    end
+  end
+
+  context 'when finding an employee (Iron Man)' do
+    before(:all) do
+      FileUtils.mkdir_p('data/avengers/tony-stark')
+    end
+
+    after(:all) do
+      FileUtils.rm_r('data/avengers')
+    end
+
+    subject { 
+      finder = (Class.new { include EmployeeFinder }).new
+      finder.find('tony') 
+    }
+
+    it 'does not find a different employee' do
+      employee = Employee.find('steve')
+      expect(employee).to be_nil
+    end
+  end
+
+  context 'with two employees with same first name (Ant Man and Beast)' do
+    before(:all) do
+      FileUtils.mkdir_p('data/avengers/hank-pym')   # Ant Man
+      FileUtils.mkdir_p('data/avengers/hank-mccoy') # Beast
+    end
+
+    after(:all) do
+      FileUtils.rm_r('data/avengers')
+    end
+
+    it 'returns the first one by alphabetical order if multiples match' do
+      beast = Employee.find('hank')
+      is_correct?(beast, 'avengers', 'Hank', 'Mccoy')
+    end
+
+    it 'finds the expected employee when given a unique key' do
+      ant_man = Employee.find('hank-p')
+      is_correct?(ant_man, 'avengers', 'Hank', 'Pym')
+    end
+  end
+end
