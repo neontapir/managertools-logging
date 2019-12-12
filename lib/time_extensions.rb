fbba2d4597fa6192827b5a5bd565e979cdb2dc6c
Chuck Durfee <cdurfee@vertafore.com>
2019-12-12 14:59:38 -0700

    fixup! update code docs

--- lib/time_extensions.rb
+++ lib/time_extensions.rb
@@ -5,14 +5,14 @@ module TimeExtensions
   refine Time do
     # Format a date in our specific format
     # @example Format a date
-    #   format_short_date(Time.new(2002)) #=> 'January  1, 2002'
+    #   Time.new(2002).short_date #=> 'January  1, 2002'
     def short_date
       self.strftime '%B %e, %Y'
     end
 
     # Format a date in our specific format
     # @example Format a date
-    #   Time.new(2002).format_date #=> 'January  1, 2002, 12:00 AM'
+    #   Time.new(2002).standard_format #=> 'January  1, 2002, 12:00 AM'
     def standard_format
       self.strftime '%B %e, %Y, %l:%M %p'
     end
