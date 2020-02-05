b08446568c270f6c41e24b920bf065a1014b22ea
Chuck Durfee <cdurfee@vertafore.com>
2020-02-05 15:54:04 -0700

    pass in settings instead of monkey patching

--- lib/commands/init_command.rb
+++ lib/commands/init_command.rb
@@ -6,15 +6,22 @@ require_relative '../settings'
 
 # Create default config file
 class InitCommand < MtCommand
+  attr_reader :settings
+
+  def initialize(settings = Settings)
+    @settings = settings
+    super()
+  end
+
   # command(arguments, options)
   #   Create a default config file in the configured location
   def command(_ = nil, _ = nil)
-    config_file = Settings.config_file
+    config_file = settings.config_file
     if File.exist?(config_file)
       warn HighLine.color("Aborting, config file #{config_file} already exists", :red)
     else
-      FileUtils.mkdir_p(Settings.root)
-      IO.write(config_file, Settings.default_config)
+      FileUtils.mkdir_p(settings.root)
+      IO.write(config_file, settings.default_config)
       raise IOError, "Failed to create settings file #{config_file}" unless File.exist? config_file
 
       puts "Initializing MT, created #{config_file}"
--- spec/init_command_spec.rb
+++ spec/init_command_spec.rb
@@ -13,32 +13,16 @@ RSpec.describe InitCommand do
   end
 
   context 'config file does not exist' do
-    before :all do
-      class Settings
-        class << self
-          undef :fake_root if method_defined? :fake_root
-          def fake_root
-            'test_init_command'
-          end
-
-          alias_method :real_root, :root
-          alias_method :root, :fake_root
-        end
-      end
-    end
-
-    after :all do
-      class Settings
-        class << self
-          alias_method :root, :real_root
-        end
+    class FakeSettings < Settings
+      def self.root
+        'test_init_command'
       end
     end
 
     it 'will initialize the data structure' do
-      config_file = Settings.config_file
+      config_file = FakeSettings.config_file
       expect(File.exist?(config_file)).to be_falsey
-      expect { init_command.command }.to output(/Initializing MT/).to_stdout.and output('').to_stderr
+      expect { InitCommand.new(FakeSettings).command }.to output(/Initializing MT/).to_stdout.and output('').to_stderr
       expect(File.exist?(config_file)).to be_truthy
       FileUtils.rm_r config_file
       expect(File.exist?(config_file)).to be_falsey
